0b035c723c9762fc1317dc252ee8800a583ba602
Satish Kumar <satishkumarmehto7@gmail.com>
2024-01-16 13:20:54 +0530

    initial version commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings: ["FullStack Developer", "Problem Solver", "TechGeek", "Workaholic"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+});
